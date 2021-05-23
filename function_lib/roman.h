850f38d0bc969b64caea87a56219b8aac344a72b
Kate1900 <61184952+Kate1900@users.noreply.github.com>
2021-05-23 20:39:51 +0300

    roman

---
+++ function_lib/roman.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <iostream>
+
+#include <map>
+
+#include <string>
+
+struct arabic
+{
+	int value;
+  
+};
+
+struct roman
+{
+	std::string value;
+  
+};
+
+class converter
+{
+public:
+
+	arabic toArabic(roman num);
+  
+	roman toRoman(arabic num);
+
+	bool check(roman& num);
+};
+
