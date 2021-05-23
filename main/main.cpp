27411ce85406d721543ebb6c328f27f670b8a790
Kate1900 <61184952+Kate1900@users.noreply.github.com>
2021-05-23 20:14:25 +0300

    main

--- main/main.cpp
+++ main/main.cpp
@@ -1,9 +1,39 @@
-#include <stdio.h>
-#include "multiply.h"
+#include <iostream>
 
-int main() {
-  printf("hello!\n");
-  printf("%d\n", multiply(3, 4));
+#include <locale.h>
 
-  return 0;
+#include "polynom.h"
+
+using namespace std;
+
+int main()
+{
+	TMonomial A(121, 5);
+  
+	TMonomial B(1214, 2);
+  
+	cout << " Monom A = " << A << endl;
+  
+	cout << " Monom B = " << B << endl;
+
+	TPolynomial P(3);
+  
+	P += A;
+  
+	P += B;
+  
+	cout << "\n Polynom P = A + B = " << P << endl;
+
+	TPolynomial P2(P);
+  
+	cout << " Polynom P2(P) = " << P2 << endl;
+
+	TPolynomial P3(3);
+  
+	P3 = P + P2;
+  
+	cout << "\n Polynom P3 = P + P2 = " << P3 << endl;
+
+	return 0;
+  
 }
