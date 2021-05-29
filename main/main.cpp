a3195aeac7c28c8986a1859544b57c14353cf4e0
Kate1900 <61184952+Kate1900@users.noreply.github.com>
2021-05-29 22:40:28 +0300

    it's better like this

--- main/main.cpp
+++ main/main.cpp
@@ -6,19 +6,21 @@ using namespace std;
 
 int main() {
 
-	arabic num = { 1 };
-  
-	roman num2 = { "IIIII" };
+	arabic num = { 3999 };
+
+	roman num2 = { "VII" };
 
 	converter conv;
 
-	cout << num.value << endl;
-  
-	cout << conv.toArabic(num2).value << endl;
-  
-	cout << conv.check(num2) << endl;
-  
-	cout << num2.value << endl;
+	cout << "Your entered arabic value is "<<num.value << endl;
+
+	cout <<"If we convert it to Roman it will be = " << conv.toRoman(num).value << endl;
+	
+	cout <<"Let's check if you entered roman value right (1 - it's correct, 0 - it's wrong). So : "<< conv.check(num2) << endl;
+
+	cout <<"Your entered roman value is "<< num2.value << endl;
+
+	cout <<"If we convert your entered roman value to Arabic it will be = "<< conv.toArabic(num2).value << endl;
   
 	return 0;
 }
